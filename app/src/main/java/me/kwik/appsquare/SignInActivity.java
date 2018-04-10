5e44a4dcc73c53a23afe22da556f336daf945952
Daniel Nistor <danielnistor2002@gmail.com>
2018-04-10 10:01:11 +0300

    KWIKDEMO-5444    IPM - Android App - Login API - app sends a non-real location

--- app/src/main/java/me/kwik/appsquare/SignInActivity.java
+++ app/src/main/java/me/kwik/appsquare/SignInActivity.java
@@ -1,7 +1,14 @@
 package me.kwik.appsquare;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -9,17 +16,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.kwik.bl.KwikMe;
 import me.kwik.bl.KwikServerError;
+import me.kwik.data.KwikLocation;
 import me.kwik.listeners.LoginListener;
 import me.kwik.rest.responses.LoginResponse;
 import me.kwk.utils.Utils;
 
-public class SignInActivity extends BaseActivity {
+public class SignInActivity extends BaseActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
+    private static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 03;
     private static int PASSWORD_MIN_LENGTH = 6;
     private View.OnClickListener mOnSignInClickListener;
     private String mEmailString;
@@ -32,8 +46,8 @@ public class SignInActivity extends BaseActivity {
 
     private Application mApp;
     private int mCountLoginErrors;
-
-
+    private Location mLastLocation;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,7 +88,11 @@ public class SignInActivity extends BaseActivity {
                     return;
                 }
                 showProgressBar();
-                KwikMe.login(mEmailString, mPasswordString, new LoginListener() {
+                KwikLocation location = null;
+                if(mLastLocation != null) {
+                    location = new KwikLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(), mLastLocation.getAccuracy());
+                }
+                KwikMe.login(mEmailString, mPasswordString, location, new LoginListener() {
 
                     @Override
                     public void onLoginCompleted(LoginResponse response) {
@@ -140,5 +158,66 @@ public class SignInActivity extends BaseActivity {
                 mPasswordString = s.toString().trim();
             }
         });
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            mGoogleApiClient.disconnect();
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(SignInActivity.this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_ID_MULTIPLE_PERMISSIONS);
+        }else{
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_ID_MULTIPLE_PERMISSIONS: {
+                if (grantResults.length == 1
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                }
+                return;
+            }
+        }
     }
 }
--- app/src/main/java/me/kwik/appsquare/SplashActivity.java
+++ app/src/main/java/me/kwik/appsquare/SplashActivity.java
@@ -5,23 +5,29 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import me.kwik.bl.KwikMe;
 import me.kwik.bl.KwikServerError;
 import me.kwik.data.KwikLocation;
-import me.kwik.listeners.HandShakeListener;
 import me.kwik.listeners.InitialHandShakeListener;
 import me.kwik.listeners.LoginListener;
 import me.kwik.rest.responses.InitialHandShakeResponse;
 import me.kwik.rest.responses.LoginResponse;
 
-public class SplashActivity extends BaseActivity  {
+public class SplashActivity extends BaseActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 03;
     private static final int REQUEST_ID_WRITE_EXTERNAL_STORAGE_PERMISSIONS = 04;
-    //private GoogleApiClient mGoogleApiClient;
+    private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
     private String TAG = SplashActivity.class.getSimpleName();
     private Application mApp;
@@ -32,7 +38,34 @@ public class SplashActivity extends BaseActivity  {
         setContentView(R.layout.activity_splash);
         mApp = (Application) getApplication();
         getSupportActionBar().hide();
+
         init();
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            mGoogleApiClient.disconnect();
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
     }
 
     private void init() {
@@ -50,7 +83,11 @@ public class SplashActivity extends BaseActivity  {
 
 
         if (KwikMe.USER_EMAIL != null || KwikMe.USER_PASSWROD != null) {
-            KwikMe.login(KwikMe.USER_EMAIL, KwikMe.USER_PASSWROD, new LoginListener() {
+            KwikLocation location = null;
+            if(mLastLocation != null) {
+                location = new KwikLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(), mLastLocation.getAccuracy());
+            }
+            KwikMe.login(KwikMe.USER_EMAIL, KwikMe.USER_PASSWROD, location, new LoginListener() {
                 @Override
                 public void onLoginCompleted(LoginResponse response) {
                     mApp.setUser(response.getUser());
@@ -88,7 +125,11 @@ public class SplashActivity extends BaseActivity  {
                     showOneButtonErrorDialog(message.getTitle(), message.getMessage());
                 } else {
                     if (KwikMe.USER_EMAIL != null || KwikMe.USER_PASSWROD != null) {
-                        KwikMe.login(KwikMe.USER_EMAIL, KwikMe.USER_PASSWROD, new LoginListener() {
+                        KwikLocation location = null;
+                        if(mLastLocation != null) {
+                            location = new KwikLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(), mLastLocation.getAccuracy());
+                        }
+                        KwikMe.login(KwikMe.USER_EMAIL, KwikMe.USER_PASSWROD, location, new LoginListener() {
                             @Override
                             public void onLoginCompleted(LoginResponse response) {
                                 Intent i = new Intent(SplashActivity.this,ClientsActivity.class);
@@ -159,6 +200,44 @@ public class SplashActivity extends BaseActivity  {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(SplashActivity.this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_ID_MULTIPLE_PERMISSIONS);
+        }else{
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_ID_MULTIPLE_PERMISSIONS: {
+                if (grantResults.length == 1
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                }
+                return;
+            }
+        }
     }
 
 
